1220acbe51441aa6c59a77b7ebde7eaa69857edc
Ajmal Rasheed <aliajmalrashid@gmail.com>
2021-02-14 23:49:15 +0500

    I have performed some function in the code.

--- testing.r
+++ testing.r
@@ -1,3 +1,12 @@
 #this is from git
 print("Hello from git hub")
 print('my directory is live')
+
+x <- c(1.7,'A')
+class(x)
+y = vector('logical', length = 10)
+y
+
+z <- 0:6
+class(z)
+as.numeric(z)
